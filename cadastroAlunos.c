8aee0f6bc7287ec27bc5206e19196d3f82b15821
jaoo10 <j.marcus.faria@gmail.com>
2014-04-24 16:24:44 -0300

    Inicio

--- cadastroAlunos.c
+++ cadastroAlunos.c
@@ -6,57 +6,122 @@
 #define DELIM_STR "|"
 
 int main(){
+	
+	int escolha_menu;
+	
+	while((escolha_menu = menu()) < 6){
 
+		switch(escolha_menu){
+			
+			case 1:
+				importarDados();
+			case 2:
+				inserirAluno();
+		}		
+
+	}
 
 }
 
 int menu(){
 
-     int escolha;
-     char resposta[10];
+	int escolha;
+	char resposta[10];
 
-     printf("\n\n\n\n\t PROGRAMA PARA CADASTRO DE ALUNOS CANDIDATOS AO VESTIBULAR\n");
-     printf("\n\nSelecione uma opção:\n\n");
-     printf("\t1. Importar dados\n");
-     printf("\t2. Inserir um novo aluno \n");
-     printf("\t3. Remover um aluno \n");
-     printf("\t4. Buscar ou exibir um aluno \n");
-     printf("\t5. Compactar o arquivo de registros \n");
-     printf("\t6. Terminar o programa\n\n");
-     printf("Digite o numero da sua escolha: ");
-     gets(resposta);
-     escolha = atoi(resposta);
-     return(escolha);
+	printf("\n\n\n\n\t PROGRAMA PARA CADASTRO DE ALUNOS CANDIDATOS AO VESTIBULAR\n");
+	printf("\n\nSelecione uma opção:\n\n");
+	printf("\t1. Importar dados\n");
+	printf("\t2. Inserir um novo aluno \n");
+	printf("\t3. Remover um aluno \n");
+	printf("\t4. Buscar ou exibir um aluno \n");
+	printf("\t5. Compactar o arquivo de registros \n");
+	printf("\t6. Terminar o programa\n\n");
+	printf("Digite o numero da sua escolha: ");
+	gets(resposta);
+	escolha = atoi(resposta);
+	return(escolha);
 
 }
 
+int lerCampo(char cct,FILE *fd,char buffer[MAX_REC_SIZE +1]){
+
+	while (cct != '\t'){
+		strcat(buffer,cct);
+		cct = fgetc(fd);
+	strcat(DELIM_STR,buffer);
+	}
+}
+
+int importarDados(){
+
+	FILE *arqDados,*arqDestino;
+
+	char nomeDestino[] = "registros.txt";
+	char nomeDados[] = "dados.txt";
+	char recbuff[MAX_REC_SIZE +1];
+	char c;
+	char tamanho[3];
+	int t = 0;
+
+	arqDados = fopen(nomeDados,"r");
+	arqDestino = fopen(nomeDestino,"w");
+
+	c = fgetc(arqDados);
+	
+	while(c != EOF){
+	
+		recbuff[0] = '\0';
+		while(c != '\n'){
+			lerCampo(c,arqDados,recbuff);
+		}
+		t = strlen(recbuff);
+		sprintf(tamanho,t);
+		fputs(tamanho,arqDestino);
+		fputs(DELIM_STR,arqDestino);
+		fputs(recbuff,arqDestino);
+	}
+	
+	fclose(arqDados);
+	fclose(arqDestino);
+
+}
+
+
 int inserirAluno(){
-    char inscricao[6],nome[],curso[],notaFinal[];
-    char recbuff[MAX_REC_SIZE +1];
+	FILE *arqDestino;
+	char nomeDestino[] = "registros.txt";
 
-    printf("\n\nDigite o numero de inscricao do aluno\n>>>");
-    gets(inscricao);
+	arqDestino = fopen(nomeDestino,"a+");
+	
+	char inscricao[6],nome[MAX_REC_SIZE],curso[MAX_REC_SIZE],notaFinal[MAX_REC_SIZE];
+	char recbuff[MAX_REC_SIZE +1];
 
-    //verificar se já existe o numero de inscricao
+	printf("\n\nDigite o numero de inscricao do aluno\n>>>");
+	gets(inscricao);
 
-    recbuff[0] = '\0';
-    strcat(recbuff,inscricao)
-    strcat(recbuff, DELIM_STR);
+	//verificar se já existe o numero de inscricao
 
-    printf("\nDigite o Nome do aluno\n>>>");
-    gets(nome);
-    strcat(recbuff,nome);
-    strcat(recbuff, DELIM_STR);
+	recbuff[0] = '\0';
+	strcat(recbuff,inscricao);
+	strcat(recbuff, DELIM_STR);
 
-    printf("\nDigite o curso que este aluno concorreu\n>>>");
-    gets(curso);
-    strcat(recbuff,curso);
-    strcat(recbuff, DELIM_STR);
+	printf("\nDigite o Nome do aluno\n>>>");
+	gets(nome);
+	strcat(recbuff,nome);
+	strcat(recbuff, DELIM_STR);
 
-    printf("\nDigite a nota final obtida pelo aluno\n>>>");
-    gets(notaFinal);
-    strcat(recbuff,notaFinal);
-    strcat(recbuff, DELIM_STR);
+	printf("\nDigite o curso que este aluno concorreu\n>>>");
+	gets(curso);
+	strcat(recbuff,curso);
+	strcat(recbuff, DELIM_STR);
+
+	printf("\nDigite a nota final obtida pelo aluno\n>>>");
+	gets(notaFinal);
+	strcat(recbuff,notaFinal);
+	strcat(recbuff, DELIM_STR);
+
+	fputs(recbuff,arqDestino);
+	fclose(arqDestino);
 
 
 }
